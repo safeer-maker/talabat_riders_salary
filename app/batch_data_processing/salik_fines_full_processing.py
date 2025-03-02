a7b52dea5242be5c8ed71e3470add3ddd50f4379
Safeer Ahmad <bsee15055@itu.edu.pk>
2025-03-02 22:00:02 +0500

    Salik processing completed

--- app/batch_data_processing/salik_fines_full_processing.py
+++ app/batch_data_processing/salik_fines_full_processing.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import os
-from datetime import datetime
 
 class SalikFinesFullProcessing:
     def __init__(self, sheet_path_name):
@@ -8,9 +7,6 @@ class SalikFinesFullProcessing:
         self.data_path      = sheet_path_name
         self.excel_file     = ""
         self.loaded_data    = pd.DataFrame()
-        self.output_path    = 'data/salik_fines_processed'
-        self.output_file    = 'salik_fines_full_processed.csv'
-        self.output_columns = ['plate', 'fine_date', 'fine_amount', 'fine_location', 'fine_status', 'fine_description']
         self.sheet_name     = ''
         self.df_error       = pd.DataFrame()
         self.__sheet        = 'salik'
@@ -33,7 +29,7 @@ class SalikFinesFullProcessing:
     def __get_sheet_name(self, excel_file):
         # Identify the sheet name that contains 'salik'
         salik_sheets = [sheet for sheet in excel_file.sheet_names if self.__sheet in sheet.lower()]
-        print(salik_sheets)
+        print("Sheets name:",salik_sheets[0])
         self.sheet_name = salik_sheets[0]
 
     def check_na(self):
@@ -41,11 +37,12 @@ class SalikFinesFullProcessing:
         self.df_error = self.loaded_data[self.loaded_data.isna().any(axis=1)]
         print("Number of entries with missing values: ", self.df_error.shape[0])
         return self.df_error.index.values + 3
-    
+
     def return_error_rows(self):
         # Return rows with errors if any
         if len(self.df_error) > 0:
             return self.df_error
+
 
 if __name__ == '__main__':
     # Path to the Excel sheet
